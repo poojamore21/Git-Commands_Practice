36656973770b7282fc6fcfc0e3f0e6a5a4813c8a
Pooja More <poojamore21112000@gmail.com>
2025-01-12 20:48:08 +0530

    Home page is updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const Home = () => {
   return (
     <div>
+      <h1>Home Page</h1>
       <p>this is home pages</p>
+      <p>Welcome To my home page</p>
+    
     </div>
   );
 };
